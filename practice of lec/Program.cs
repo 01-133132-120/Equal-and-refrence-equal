ef534f65aafa7d4c586b3fa4a27e619b6af973c9
hassan <hassan26khan@gmail.com>
2016-03-15 00:10:39 +0500

    Equal & Reference Equal

---
+++ practice of lec/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_of_lec
+{
+    class person
+    {//Strat of person class
+        public String getname(String name)
+        {//Start of get name fuction
+           
+            return name;
+
+        }//end of get name function
+        public int getage(int age)
+        {//Start of get age function
+           
+            return age;
+        }//end of getage function
+    }//End of person class
+    class child
+    {//start of child class
+        public String getname(String name)
+        {//Start of get name fuction
+
+            return name;
+
+        }//end of get name function
+        public int getage(int age)
+        {//Start of get age function
+
+            return age;
+        }//end of getage function
+
+    }//end of child class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person obj = new person();
+            child obj1=new child();
+           String name=  obj.getname("hassan");
+           int age = obj.getage(22);
+           String nameChild = obj1.getname("khan");
+           
+           int ageChild = obj1.getage(22);
+           Console.WriteLine("comparing Names");
+           Console.WriteLine(name.Equals(nameChild));
+           //name = nameChild;
+          Console.WriteLine( "Output of refrence equals : " + ReferenceEquals(name, nameChild));
+          name = nameChild;
+          Console.WriteLine("Output of refrence equals after  : " + ReferenceEquals(name, nameChild));
+           Console.WriteLine("comparing Ages");
+           Console.WriteLine(age.Equals(ageChild));
+           int age1 = obj.getage(21);
+           Console.WriteLine("When Ages are not equal  "+age1.Equals(ageChild));
+           Console.ReadLine();
+        }
+    }
+}
---
+++ practice of lec/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("practice of lec")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("practice of lec")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c41c3376-276f-40e9-9d72-af4dd565436d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
